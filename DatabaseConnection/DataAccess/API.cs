441cfce918f51ac6227b355c6c8015e8daee7f75
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2020-11-25 09:41:35 +0100

    Changed API to static connection

--- DatabaseConnection/DataAccess/API.cs
+++ DatabaseConnection/DataAccess/API.cs
@@ -7,36 +7,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseConnection
 {
-    public class API
+    public static class API
     {
-        public static List<Movie> GetMovieSlice(int a, int b)
+
+        static Context ctx;
+
+        static API()
         {
-            using var ctx = new Context();
-            return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
+            ctx = new Context();
         }
+
+        public static List<Movie> GetMovieSlice(int a, int b)
+        {           
+            return ctx.Movies.OrderBy(m => m.Title)
+                .Skip(a)
+                .Take(b)
+                .ToList();
+        }
+
         public static Customer GetCustomerByUserName(string userName)
-        {
-            using var ctx = new Context();
-            return ctx.Customers.FirstOrDefault(c => c.UserName.ToLower() == userName.ToLower());
+        {            
+            return ctx.Customers
+                .FirstOrDefault(c => c.UserName.ToLower() == userName.ToLower());
         }
 
         public static bool RegisterSale(Customer customer, List<Movie> movies)
-        {
-            using var ctx = new Context();
+        {            
             try
-            {
-                // Här säger jag åt contextet att inte oroa sig över innehållet i dessa records.
-                // Om jag inte gör detta så kommer den att försöka updatera databasens Id och cracha.
-                ctx.Entry(customer).State = EntityState.Unchanged;
-                //ctx.Entry(movies).State = EntityState.Unchanged;
-
-                foreach (var movie in movies)
-                {
-                    ctx.Entry(movie).State = EntityState.Unchanged;
-                }
+            {               
 
                 ctx.Add(new Rental() { Date = DateTime.Now, Customer = customer, Movies = movies });
-                return ctx.SaveChanges() == 1;
+                bool one_record_added =  ctx.SaveChanges() == 1;
+                return one_record_added;
             }
             catch (DbUpdateException e)
             {
@@ -45,38 +47,6 @@ namespace DatabaseConnection
                 return false;
             }
         }
-    }
-
-    //public static class API
-    //{
-    //    static Context ctx;
-
-    //    static API()
-    //    {
-    //        ctx = new Context();
-    //    }
-    //    public static List<Movie> GetMovieSlice(int a, int b)
-    //    {
-    //        return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
-    //    }
-    //    public static Customer GetCustomerByName(string name)
-    //    {
-    //        return ctx.Customers.FirstOrDefault(c => c.FirstName.ToLower() == name.ToLower());
-    //    }
-    //    public static bool RegisterSale(Customer customer, List<Movie> movie)
-    //    {
-    //        try
-    //        {
-    //            ctx.Add(new Rental() { Date = DateTime.Now, Customer = customer, Movies = movie });
-    //            return ctx.SaveChanges() == 1;
-    //        }
-    //        catch (DbUpdateException e)
-    //        {
-    //            System.Diagnostics.Debug.WriteLine(e.Message);
-    //            System.Diagnostics.Debug.WriteLine(e.InnerException.Message);
-    //            return false;
-    //        }
-    //    }
-    //}
+    }   
 
 }

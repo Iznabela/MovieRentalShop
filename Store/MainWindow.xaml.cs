98709f5584ef10fc81f69e26c400d37b66bdf437
Isabella Pettersson <isabella.pettersso1@learnet.se>
2020-12-04 19:25:46 +0100

    bugfix to be able to print more movies

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -159,7 +159,7 @@ namespace Store
                              e is System.IO.FileNotFoundException ||
                              e is UriFormatException)
                         {
-
+                            i++;
                             continue;
                         }
                     }
